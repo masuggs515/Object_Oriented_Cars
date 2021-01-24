46bd7675895938f4b293352f63c6e8078783b936
Michael <masuggs515@gmail.com>
2021-01-24 00:02:05 -0500

    removed instruction

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// OO Challenge
-
 class Vehicle {
     constructor(make, model, year){
         this.make = make;
@@ -51,26 +49,3 @@ class Garage {
             return "Vehicle added!"
     }
 }
-// Part Four
-
-
-// Create a class for a Garage. It should have a property called vehicles which will store an array of vehicles, 
-// and a property called capacity which is a number indicating how many vehicles will fit in the garage. 
-// When you create a garage, vehicles will always be empty; you only need to provide the capacity.
-
-// A garage should also have an add method, which attempts to add a vehicle to the array of vehicles. 
-// However, if you try to add something which is not a vehicle, the garage should return the message 
-// “Only vehicles are allowed in here!”. Also, if the garage is at capacity, it should say “Sorry, we’re full.”
-
-// let garage = new Garage(2);
-// garage.vehicles; // []
-// garage.add(new Car("Hyundai", "Elantra", 2015)); // "Vehicle added!"
-// garage.vehicles; // [Car]
-// garage.add("Taco"); // "Only vehicles are allowed in here!"
-
-// garage.add(new Motorcycle("Honda", "Nighthawk", 2000));
-// // "Vehicle added!"
-// garage.vehicles; // [Car, Motorcycle]
-
-// garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
-// // "Sorry, we're full."
